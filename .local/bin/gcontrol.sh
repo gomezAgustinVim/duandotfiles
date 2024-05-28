e94e26f494be683ff60aed04e3d6311f9e96e753
Agustin Gomez <niconecrofilo589@gmail.com>
2024-05-28 11:51:05 -0300

    currently reworking it and deleting stupid shit

--- .local/bin/gcontrol.sh
+++ .local/bin/gcontrol.sh
@@ -2,17 +2,11 @@
 
 file=$HOME/Documentos/tokengit.txt
 
-# adds everything and makes a commit
-adding() {
-    git add .
+# makes a commit
+committing() {
     read -p "Mensaje para el commit: " commit_message
     read -p "Archivo para el commit: " commit_file
     git commit -m "$commit_message" $commit_file
-}
-
-updating() {
-  git add .
-  git commit --amend --no-edit
 }
 
 pushing() {
@@ -24,8 +18,7 @@ pushing() {
 ask() {
     choice=$(printf "Add\\nUpdate\\nPush" | dmenu -i -p "Qué opción desea ejecutar?")
     case "$choice" in
-        Add) adding ;;
-        Update) updating ;;
+        Commit) committing ;;
         Push) pushing ;;
     esac
 }
