56e60fa58e99f2996522669507dd98f678c14d7c
Agustin Gomez <niconecrofilo589@gmail.com>
2024-06-26 17:38:11 -0300

    Some better menu and interaction within many of the scripts

--- .local/bin/gcontrol.sh
+++ .local/bin/gcontrol.sh
@@ -2,25 +2,6 @@
 
 file=$HOME/Documentos/tokengit.txt
 
-# makes a commit
-committing() {
-    read -p "Mensaje para el commit: " commit_message
-    read -p "Archivo para el commit: " commit_file
-    git commit -m "$commit_message" $commit_file
-}
-
-pushing() {
-    cat $file | xargs -I '{}' echo -n {} | xclip -sel clip
-    git push
-    echo "" | xclip -sel clip # empties xclip for safety
-}
-
-ask() {
-    choice=$(printf "Add\\nUpdate\\nPush" | dmenu -i -p "Qué opción desea ejecutar?")
-    case "$choice" in
-        Commit) committing ;;
-        Push) pushing ;;
-    esac
-}
-
-ask
+cat $file | xargs -I '{}' echo -n {} | xclip -sel clip
+git push
+echo "" | xclip -sel clip # empties xclip for safety
--- .local/bin/mover_multimedia.sh
+++ .local/bin/mover_multimedia.sh
@@ -4,35 +4,27 @@ IMAGEN=$HOME/Imagenes
 VIDEO=$HOME/Videos
 
 # Mueve las imagenes a donde van las imagenes
-# Chequea que el directorio exista
-if [ ! -d $HOME/Imagenes ]; then
-    mkdir $HOME/Imagenes
-fi
-for i in *.{jpg,png,webp}; do
-    mv "$i" $HOME/Imagenes
-done
-notify-send "Imagenes movidas a $IMAGEN con éxito"
+mov_imagen() {
+    for i in *.{jpg,png,webp}; do
+        [ -e "$i" ] && mv "$i" $IMAGEN 2>/dev/null
+    done
+    notify-send "Imagenes movidas a $IMAGEN con éxito"
+}
 
 # Mueve los videos a donde tienen que ir
-if [ ! -d $HOME/Videos ]; then
-    mkdir $HOME/Videos
-fi
-for i in *.{avi,mp4,webm}; do
-    mv "$i" $HOME/Videos
-done
-notify-send "Videos movidos a $VIDEO con éxito"
+mov_video() {
+    for i in *.{avi,mp4,webm}; do
+        [ -e "$i" ] && mv "$i" $VIDEO 2>/dev/null
+    done
+    notify-send "Videos movidos a $VIDEO con éxito"
+}
 
-# Windows syntax
+ask() {
+    choice=$(printf "Imagenes\\nVideos" | dmenu -i -p "Qué desea mover?")
+    case "$choice" in
+        Imagenes) mov_imagen ;;
+        Videos) mov_video ;;
+    esac
+}
 
-# @echo off
-#
-# set IMAGEN=%USERPROFILE%\Imagenes
-# set VIDEO=%USERPROFILE%\Videos
-#
-# if not exist %IMAGEN% mkdir %IMAGEN%
-# for %I in (*.jpg,*.png,*.webp) do move /Y "%I" %IMAGEN%
-# echo "Imagenes movidas a %IMAGEN% con exito"
-#
-# if not exist %VIDEO% mkdir %VIDEO%
-# for %I in (*.avi,*.mp4,*.webm) do move /Y "%I" %VIDEO%
-# echo "Videos movidos a %VIDEO% con exito"
+ask
