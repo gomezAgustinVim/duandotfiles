8dda8d6218cabfdd7a11b8014f7f6aeebb6b2071
Agustin Gomez <niconecrofilo589@gmail.com>
2024-09-05 00:09:22 -0300

    Zathura config can be changed effortlessly via toggle script

--- .local/bin/chosen_zathura_theme.sh
+++ .local/bin/chosen_zathura_theme.sh
@@ -17,29 +17,12 @@ DARK_THEME="zathura-gruvbox-dark-soft"
 #ESCAPED_LIGHT_THEME=$(printf '%s\n' "$LIGHT_THEME" | sed 's/[]\/$*.^[]/\\&/g')
 #ESCAPED_DARK_THEME=$(printf '%s\n' "$DARK_THEME" | sed 's/[]\/$*.^[]/\\&/g')
 
-claro() {
  if grep -q "$LIGHT_THEME" "$CONFIG_FILE"; then
-    dunstify -u critical "El tema claro ya está activado"
-    exit 1
-  fi
-
-  sed -i "/$DARK_THEME/d" "$CONFIG_FILE"  # Remove the dark theme line if it exists
-  echo 'include ./'"$LIGHT_THEME" >> "$CONFIG_FILE"   # Append the light theme line
+    sed -i "/$LIGHT_THEME/d" "$CONFIG_FILE"
+    echo 'include ./'"$DARK_THEME" >> "$CONFIG_FILE"
+    dunstify -u normal -t 3600 "Cambiado al tema oscuro"
+else
+  sed -i "/$DARK_THEME/d" "$CONFIG_FILE"
+  echo 'include ./'"$LIGHT_THEME" >> "$CONFIG_FILE"
   dunstify -u normal -t 3600 "Cambiado al tema claro"
-}
-
-oscuro() {
- if grep -q "$DARK_THEME" "$CONFIG_FILE"; then
-    dunstify -u critical "El tema oscuro ya está activado"
-    exit 1
-  fi
-
-  sed -i "/$LIGHT_THEME/d" "$CONFIG_FILE"
-  echo 'include ./'"$DARK_THEME" >> "$CONFIG_FILE"     # Append the dark theme line
-  dunstify -u normal -t 3600 "Cambiado al tema oscuro"
-}
-
-case $(printf "Tema claro\\nTema oscuro" | dmenu -l 2 -i -p "Qué tema de zathura usar?") in
-    "Tema claro") claro ;;
-    "Tema oscuro") oscuro ;;
-esac
+fi
