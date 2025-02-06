0467ff323d613fdceafb9c4891aa3b3656170f35
Agustin Gomez <niconecrofilo589@gmail.com>
2025-02-06 15:59:00 -0300

    autocmd

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,66 +1,67 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = 'BufWritePre', -- uncomment for format on save
-        opts = require "configs.conform",
-    },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
-    -- These are some examples, uncomment them if you want to see them work!
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "configs.lspconfig"
-        end,
-    },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "vim", "lua", "vimdoc",
-                "html", "css"
-            },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+
+  -- Productivity
+  {
+    "vimwiki/vimwiki",
+    init = function()
+      -- Define file extensions to
+      -- syntax mappings for Vimwiki
+      vim.g.vimwiki_list = {
+        {
+          path = "~/.local/share/vimwiki",
+          syntax = "markdown",
+          ext = ".md",
         },
-    },
+      }
 
-    -- Productivity
-    {
-        "vimwiki/vimwiki",
-        init = function()
-            -- Define file extensions to
-            -- syntax mappings for Vimwiki
-            vim.g.vimwiki_list = {
-                {
-                    path = '~/.local/share/vimwiki',
-                    syntax = 'markdown',
-                    ext = '.md',
-                },
-            }
+      vim.g.vimwiki_ext2syntax = {
+        [".md"] = "markdown",
+        [".Rmd"] = "markdown",
+        [".rmd"] = "markdown",
+        [".markdown"] = "markdown",
+        [".mdown"] = "markdown",
+      }
 
-            vim.g.vimwiki_ext2syntax = {
-                ['.md'] = 'markdown',
-                ['.Rmd'] = 'markdown',
-                ['.rmd'] = 'markdown',
-                ['.markdown'] = 'markdown',
-                ['.mdown'] = 'markdown',
-            }
+      -- This will make sure vimwiki will only set the
+      -- filetype of markdown files inside a wiki
+      -- directory, rather than vim.globally.
+      vim.g.vimwiki_global_ext = 0
+    end,
+  },
 
-            -- This will make sure vimwiki will only set the
-            -- filetype of markdown files inside a wiki
-            -- directory, rather than vim.globally.
-            vim.g.vimwiki_global_ext = 0
-        end,
-    },
+  "lukas-reineke/indent-blankline.nvim",
+  "ryanoasis/vim-devicons",
 
-    "lukas-reineke/indent-blankline.nvim",
-    "ryanoasis/vim-devicons",
+  -- Syntax Highlighting and Colors --
+  "PotatoesMaster/i3-vim-syntax",
+  "kovetskiy/sxhkd-vim",
 
-    -- Syntax Highlighting and Colors --
-    "PotatoesMaster/i3-vim-syntax",
-    "kovetskiy/sxhkd-vim",
-    "ap/vim-css-color",
-
-    -- Junegunn Choi Plugins --
-    "junegunn/vim-emoji",
-
+  -- Junegunn Choi Plugins --
+  "junegunn/vim-emoji",
 }
