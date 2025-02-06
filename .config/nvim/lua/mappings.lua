8dc7151c7a2d02a0df637d02c38bcba34c206264
Agustin Gomez <niconecrofilo589@gmail.com>
2025-02-06 15:56:46 -0300

    nvim mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,18 +2,18 @@ require "nvchad.mappings"
 
 -- add yours here
 
--- local map = vim.keymap.set
+local map = vim.keymap.set
 
-local function map(m, k, v)
-    vim.keymap.set(m, k, v, { silent = true, noremap = true })
-end
+-- local function map(m, k, v)
+-- vim.keymap.set(m, k, v, { silent = true, noremap = true })
+-- end
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Mimic shell movements
-map('i', '<C-E>', '<C-o>$')
-map('i', '<C-A>', '<C-o>^')
+map("i", "<C-E>", "<C-o>$")
+map("i", "<C-A>", "<C-o>^")
 
 -- God reveals himself
 -- Thanks the Primeagen
@@ -22,26 +22,30 @@ map("v", "K", ":m '<-2<CR>gv=gv")
 map("n", "J", "mzJ`z")
 
 -- Quickly save the current buffer or all buffers
-map('n', '<leader>w', '<CMD>update<CR>')
-map('n', '<leader>W', '<CMD>wall<CR>')
+map("n", "<leader>w", "<CMD>update<CR>")
+map("n", "<leader>W", "<CMD>wall<CR>")
 
 -- Quit neovim
-map('n', '<C-Q>', '<CMD>q<CR>')
+map("n", "<C-Q>", "<CMD>q<CR>")
 
 -- Save neovim
-map('n', '<C-s>', '<CMD>w<CR>')
+map("n", "<C-s>", "<CMD>w<CR>")
 
 -- Save and quit neovim
-map('n', '<C-z>', '<CMD>x<CR>')
+map("n", "<C-z>", "<CMD>x<CR>")
+
+-- Create new tab
+map("n", "<C-T>", "<CMD>tabnew<CR>")
 
 -- Shortcut to yank register
-map({ 'n', 'x' }, '<leader>py', '"0p')
+map({ "n", "x" }, "<leader>py", '"0p')
 
-map('n', '<leader>o', '<CMD>setlocal spell! spelllang=es_ar<CR>')
-map('n', '<leader>O', '<CMD>setlocal spell! spelllang=ja_jp<CR>')
+-- Activate local spell
+map("n", "<leader>o", "<CMD>setlocal spell! spelllang=es_ar<CR>")
+map("n", "<leader>O", "<CMD>setlocal spell! spelllang=ja_jp<CR>")
 
 -- Nerd tree
-map('n', '<c-n>', '<CMD>NERDTreeToggle<CR>')
+map("n", "<c-n>", "<CMD>NvimTreeToggle<CR>")
 
 ------------------------------------------------
 -- ==>   Splits
@@ -49,47 +53,63 @@ map('n', '<c-n>', '<CMD>NERDTreeToggle<CR>')
 
 -- Make splits
 -- Either vertical or horizontal
-map('n', '<Leader>vs', '<CMD>vs<CR>')
-map('n', '<Leader>hs', '<CMD>split<CR>')
+map("n", "<Leader>vs", "<CMD>vs<CR>")
+map("n", "<Leader>hs", "<CMD>split<CR>")
 
 -- Shortcutting split navigation, saving a keypress:
-map('n', '<C-h>', '<C-w>h')
-map('n', '<C-j>', '<C-w>j')
-map('n', '<C-k>', '<C-w>k')
-map('n', '<C-l>', '<C-w>l')
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
 
 -- Make adjusting split sizes a bit more friendly
-map('n', '<C-Left>', '<CMD>vertical resize +3<CR>')
-map('n', '<C-Right>', '<CMD>vertical resize -3<CR>')
-map('n', '<C-Up>', '<CMD>resize +3<CR>')
-map('n', '<C-Down>', '<CMD>resize -3<CR>')
+map("n", "<C-Left>", "<CMD>vertical resize +3<CR>")
+map("n", "<C-Right>", "<CMD>vertical resize -3<CR>")
+map("n", "<C-Up>", "<CMD>resize +3<CR>")
+map("n", "<C-Down>", "<CMD>resize -3<CR>")
 
 -- Change 2 split windows from vert to horiz or horiz to vert
-map('n', '<Leader>th', '<C-w>t<C-w>H')
-map('n', '<Leader>tk', '<C-w>t<C-w>K')
+map("n", "<Leader>th", "<C-w>t<C-w>H")
+map("n", "<Leader>tk", "<C-w>t<C-w>K")
 
 -- Replace ex mode with gq
-map('n', 'Q', 'gq')
+map("n", "Q", "gq")
 
 -- Check file in shellcheck:
 -- map('n', '<leader>s', ':!clear && shellcheck -x %<CR>')
 
 -- Compile document, be it groff/LaTeX/markdown/etc.
-map('n', '<leader>c', ':w! | !compiler "<c-r>%"<CR>')
+map("n", "<leader>c", ':w! | !compiler "<c-r>%"<CR>')
 
 -- Compile slide with pandoc
-map('n', '<leader>e', ':!pandoc % -t beamer -o %.pdf<CR>')
+map("n", "<leader>e", ":!pandoc % -t beamer -o %.pdf<CR>")
 
 -- Perform dot commands over visual blocks:
-map('x', '.', ':normal .<CR>')
+map("x", ".", ":normal .<CR>")
 
 -- Mapping to open VimwikiIndex
-map('n', '<leader>ww', ':VimwikiIndex<CR>', { noremap = true })
+map("n", "<leader>ww", ":VimwikiIndex<CR>", { noremap = true })
 
 -- Open corresponding .pdf/.html or preview
-map('n', '<leader>p', ':!opout <c-r>%<CR><CR>')
+map("n", "<leader>p", ":!opout <c-r>%<CR><CR>")
 
 -- Format a file
 map("n", "<leader>fm", vim.lsp.buf.format)
+
+map("n", "K", function()
+  vim.lsp.buf.hover()
+end)
+map("n", "gd", function()
+  vim.lsp.buf.definition()
+end)
+map("n", "gD", function()
+  vim.lsp.buf.declaration()
+end)
+map("n", "gi", function()
+  vim.lsp.buf.implementation()
+end)
+map("n", "<F2>", function()
+  vim.lsp.buf.rename()
+end)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
