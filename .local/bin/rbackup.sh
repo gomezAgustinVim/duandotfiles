765ae7485a7121e84cb431eff8ae51d37ae2171c
Agustin Gomez <niconecrofilo589@gmail.com>
2025-02-24 16:44:56 -0300

    backup utility to copy directories which appends date names and changes them accordingly if the date differs

--- .local/bin/rbackup.sh
+++ .local/bin/rbackup.sh
@@ -1,22 +1,42 @@
 #!/bin/sh
 
-# this is a work in progress
-readonly RSYNC_DEFAULTS="-Pavz"
+# Works with runit
+# Parameters for RSYNC
+# Name of USB drive
 readonly NOMBRE="$1"
-readonly DESTINO=/run/media/utane/$NOMBRE
+# My filter rules
+readonly BACKUP_FILTER="$HOME/Documentos/backup.filter"
+# File to store backup history
+readonly BACKUP_HISTORY="$XDG_DATA_HOME/backup_history.txt"
+DAY=$(date +%d-%m-%H)
+# USB PATH GOES HERE
+readonly DESTINO="/run/media/utane/$NOMBRE/backup"
+DESTINO_FINAL="$DESTINO/$DAY"
 
-case "$DESTINO" in
-    */Descargas|*/Documentos|*/Imagenes|*/Musica|*/Escritorio|*/Videos)
-        ;;
-    *)
-        mkdir -p "$DESTINO/Descargas" "$DESTINO/Documentos" "$DESTINO/Imagenes" "$DESTINO/Musica" "$DESTINO/Escritorio" "$DESTINO/Videos"
-        ;;
-esac
+# Append the current backup directory to the history file
+echo "$DAY" >> "$BACKUP_HISTORY"
 
-backup_files() {
-    # rsync $RSYNC_DEFAULTS --delete --exclude={'Series','Mega-Programacion','node_modules','.angular','.vscode','.git'} "$XDG_DATA_HOME/vimwiki" "$HOME/Descargas" "$HOME/Documentos" "$HOME/Imagenes" --exclude={'Emulation','ISO','Images','Cosas-Windows'} "$HOME/Escritorio" "$DESTINO"
-    rsync $RSYNC_DEFAULTS --delete --exclude-from='/home/utane/Documentos/excluir.txt' "$XDG_DATA_HOME/vimwiki" "$HOME/Descargas" "$HOME/Documentos" "$HOME/Imagenes" "$HOME/Escritorio" "$HOME/Musica/Nuevas" "$DESTINO"
-    notify-send "Los archivos se terminaron de mandar a $DESTINO"
-}
+# Get the previous backup directory from the history file
+# DIRECTORIO_PREVIO=$(find $DESTINO -mindepth 1 -maxdepth 1 -type d -printf '%T + %P\n' | sort | head -n 1 | cut -d' ' -f2)
+# DESTINO_PREVIO=basename $(ls -1tr "$DESTINO" | head -n 1)
+DIRECTORIO_PREVIO=$(head -n 1 "$BACKUP_HISTORY")
 
-backup_files
+# WiP
+if [ -n "$DIRECTORIO_PREVIO" ] && [ "$DIRECTORIO_PREVIO" != "$DAY" ]; then
+  # The reason I do this is because I don't care about incr backups
+  # It's not really necesary for my use case 
+  mv "$DESTINO/$DIRECTORIO_PREVIO" "$DESTINO_FINAL"
+  echo "$DAY" > "$BACKUP_HISTORY"
+else
+  # Create the backup directory for today
+  mkdir -p "$DESTINO_FINAL"
+fi
+
+rsync -av --delete --progress --filter="merge $BACKUP_FILTER" "$XDG_DATA_HOME/vimwiki" "$HOME/Descargas" "$HOME/Documentos" "$HOME/Imagenes" "$HOME/Escritorio" "$HOME/Musica/Nuevas" "$DESTINO_FINAL"
+
+ if [ $? -eq 0 ]; then
+    notify-send "Backup completado" "Los archivos se han copiado a $DESTINO_FINAL"
+  else
+    notify-send "Error en el backup" "Hubo un problema al copiar los archivos a $DESTINO_FINAL"
+  fi
+
