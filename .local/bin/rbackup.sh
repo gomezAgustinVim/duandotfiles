f7514107d3b81776343f79ffe91cd64b18fa6705
Agustin Gomez <niconecrofilo589@gmail.com>
2025-02-28 00:28:54 -0300

    Rsync uses P flag and deletes after

--- .local/bin/rbackup.sh
+++ .local/bin/rbackup.sh
@@ -9,7 +9,7 @@ readonly COPIADOS="$2"
 readonly BACKUP_FILTER="$HOME/Documentos/backup.filter"
 # File to store backup history
 readonly BACKUP_HISTORY="$XDG_DATA_HOME/backup_history.txt"
-readonly DAY=$(date +%d-%m-%H)
+readonly DAY=$(date +%y%d%m-%H-%M)
 # USB PATH GOES HERE
 readonly DESTINO="/run/media/utane/$NOMBRE/backup"
 readonly DESTINO_FINAL="$DESTINO/$DAY"
@@ -21,20 +21,19 @@ echo "$DAY" >> "$BACKUP_HISTORY"
 DIRECTORIO_PREVIO=$(head -n 1 "$BACKUP_HISTORY")
 
 if [ -n "$DIRECTORIO_PREVIO" ] && [ "$DIRECTORIO_PREVIO" != "$DAY" ]; then
-  # The reason I do this is because I don't care about incr backups
-  # It's not really necesary for my use case 
-  mv "$DESTINO/$DIRECTORIO_PREVIO" "$DESTINO_FINAL"
-  echo "$DAY" > "$BACKUP_HISTORY"
+    # The reason I do this is because I don't care about incr backups
+    # It's not really necesary for my use case
+    mv "$DESTINO/$DIRECTORIO_PREVIO" "$DESTINO_FINAL"
+    echo "$DAY" > "$BACKUP_HISTORY"
 else
-  # Create the backup directory for today
-  mkdir -p "$DESTINO_FINAL"
+    # Create the backup directory for today
+    mkdir -p "$DESTINO_FINAL"
 fi
 
 rsync -Pav --update --delete-after --filter="merge $BACKUP_FILTER" "$COPIADOS" "$DESTINO_FINAL"
 
- if [ $? -eq 0 ]; then
+if [ $? -eq 0 ]; then
     notify-send "Backup completado" "Los archivos se han copiado a $DESTINO_FINAL"
-  else
+else
     notify-send "Error en el backup" "Hubo un problema al copiar los archivos a $DESTINO_FINAL"
-  fi
-
+fi
