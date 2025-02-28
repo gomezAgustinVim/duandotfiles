f6776be9a1ab00aaf2f4170092a845f9587f058c
Agustin Gomez <niconecrofilo589@gmail.com>
2025-02-28 00:28:54 -0300

    Rsync uses P flag and deletes after

--- .local/bin/rbackup.sh
+++ .local/bin/rbackup.sh
@@ -4,24 +4,22 @@
 # Parameters for RSYNC
 # Name of USB drive
 readonly NOMBRE="$1"
+readonly COPIADOS="$2"
 # My filter rules
 readonly BACKUP_FILTER="$HOME/Documentos/backup.filter"
 # File to store backup history
 readonly BACKUP_HISTORY="$XDG_DATA_HOME/backup_history.txt"
-DAY=$(date +%d-%m-%H)
+readonly DAY=$(date +%d-%m-%H)
 # USB PATH GOES HERE
 readonly DESTINO="/run/media/utane/$NOMBRE/backup"
-DESTINO_FINAL="$DESTINO/$DAY"
+readonly DESTINO_FINAL="$DESTINO/$DAY"
 
 # Append the current backup directory to the history file
 echo "$DAY" >> "$BACKUP_HISTORY"
 
 # Get the previous backup directory from the history file
-# DIRECTORIO_PREVIO=$(find $DESTINO -mindepth 1 -maxdepth 1 -type d -printf '%T + %P\n' | sort | head -n 1 | cut -d' ' -f2)
-# DESTINO_PREVIO=basename $(ls -1tr "$DESTINO" | head -n 1)
 DIRECTORIO_PREVIO=$(head -n 1 "$BACKUP_HISTORY")
 
-# WiP
 if [ -n "$DIRECTORIO_PREVIO" ] && [ "$DIRECTORIO_PREVIO" != "$DAY" ]; then
   # The reason I do this is because I don't care about incr backups
   # It's not really necesary for my use case 
@@ -32,7 +30,7 @@ else
   mkdir -p "$DESTINO_FINAL"
 fi
 
-rsync -av --delete --progress --filter="merge $BACKUP_FILTER" "$XDG_DATA_HOME/vimwiki" "$HOME/Descargas" "$HOME/Documentos" "$HOME/Imagenes" "$HOME/Escritorio" "$HOME/Musica/Nuevas" "$DESTINO_FINAL"
+rsync -Pav --update --delete-after --filter="merge $BACKUP_FILTER" "$COPIADOS" "$DESTINO_FINAL"
 
  if [ $? -eq 0 ]; then
     notify-send "Backup completado" "Los archivos se han copiado a $DESTINO_FINAL"
