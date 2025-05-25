9e55354cb9c17a9b0a5f5c383372464c4ecf1881
Agustin Gomez <niconecrofilo589@gmail.com>
2025-05-25 01:06:00 -0300

    Wayland specific files go on this branch, they are not to be merged

---
+++ .config/waybar/scripts/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -9 waybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x waybar > /dev/null; do sleep 1; done
+
+# Launch Polybar, using config location ~/.config/bspwm/polybar/config.ini
+waybar &
