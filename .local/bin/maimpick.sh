6742fb58359fa3c049de099aef137f1fb783f364
Agustin Gomez <niconecrofilo589@gmail.com>
2025-05-30 23:31:43 -0300

    Ignore graphic acceleration so that background is not blurred

--- .local/bin/maimpick.sh
+++ .local/bin/maimpick.sh
@@ -9,10 +9,10 @@ output="$(date '+%y%m%d-%H%M-%S').png"
 xclip_cmd="xclip -sel clip -t image/png"
 
 case "$(printf "area seleccionada\\nventana actual\\npantalla completa\\narea seleccionada (copiar)\\nventana actual (copiar)\\npantalla completa (copiar)" | rofi -dmenu -l 6 -i -p "Tipo de selección")" in
-    "area seleccionada") maim -s -u pic-selected-"${output}" ;;
-    "ventana actual") maim -u -q -d 0.2 -i "$(xdotool getactivewindow)" pic-window-"${output}" ;;
-    "pantalla completa") maim -u -q -d 0.2 pic-full-"${output}" ;;
-    "area seleccionada (copiar)") maim -s -u | ${xclip_cmd} ;;
-    "ventana actual (copiar)") maim -u -q -d 0.2 -i "$(xdotool getactivewindow)" | ${xclip_cmd} ;;
-    "pantalla completa (copiar)") maim -u -q -d 0.2 | ${xclip_cmd} ;;
+    "area seleccionada") maim -s -u -o pic-selected-"${output}" ;;
+    "ventana actual") maim -u -q -d 0.2 -o -i "$(xdotool getactivewindow)" pic-window-"${output}" ;;
+    "pantalla completa") maim -u -q -d 0.2 -o pic-full-"${output}" ;;
+    "area seleccionada (copiar)") maim -s -u -o | ${xclip_cmd} ;;
+    "ventana actual (copiar)") maim -u -q -d 0.2 -o -i "$(xdotool getactivewindow)" | ${xclip_cmd} ;;
+    "pantalla completa (copiar)") maim -u -q -d 0.2 -o | ${xclip_cmd} ;;
 esac
