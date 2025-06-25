cd3b7575a1b28e5ffc690adcd67b7925aecbbf35
Agustin Gomez <niconecrofilo589@gmail.com>
2025-06-25 20:21:45 -0300

    checks if using wayland first, then uses xorg or wayland programs

--- .local/bin/markdownjp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# fuente="/home/utane/.local/share/mincho.tex"
-fuente="/home/utane/.local/share/NotoSansCJK.ttc"
-
-archivo=$1
-destino=$2
-salida=$3
-
-pandoc $archivo -s -t $destino --pdf-engine=xelatex -H $fuente -o $salida
