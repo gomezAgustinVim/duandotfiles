b04985ffc5c4cf65d23f6cbfe49ae8f383bab3bd
Agustin Gomez <niconecrofilo589@gmail.com>
2025-06-26 19:34:39 -0300

    Better options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,14 +5,9 @@
 local g = vim.g
 local o = vim.o
 
--- g.mapleader = ' '
--- g.maplocalleader = ' '
-
 -- Splits open at the bottom and right, which is non-retarded, unlike vim defaults.
 o.splitbelow = true
 o.splitright = true
-
-vim.cmd("syntax on")
 
 -- The following are commented out as they cause vim to behave a lot
 -- differently from regular Vi. They are highly recommended though.
@@ -21,42 +16,45 @@ o.showmatch = true -- Show matching brackets.
 o.ignorecase = true -- Do case insensitive matching
 o.smartcase = true -- Do smart case matching
 o.incsearch = true -- Incremental search
+o.hlsearch = false
 o.autowrite = true -- Automatically save before commands like :next and :make
 o.hidden = true -- Hide buffers when they are abandoned
 
 -- Better editing experience
-o.expandtab = true -- Turn tab into spaces
-o.smarttab = true
-o.cindent = true
-o.autoindent = true
-o.hidden = true
-o.backup = false
-o.wrap = true
-o.textwidth = 300
-o.tabstop = 4
+o.tabstop = 4 -- Tabs counts as 4 spaces
 o.shiftwidth = 4
 o.softtabstop = -1 -- If negative, shiftwidth value is used
+o.expandtab = true -- Turn tab into spaces
+o.cindent = true
+o.smartindent = true
+o.autoindent = true
+o.swapfile = false
+o.backup = false
+o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+o.undofile = true
+o.wrap = true
+o.textwidth = 300
 o.list = true
 o.listchars = "trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂"
 
 -- Better editor UI
 o.numberwidth = 5
+o.nu = true
 o.relativenumber = true
--- o.signcolumn = 'yes:2'
 o.cursorline = true
 o.cursorlineopt = "both" -- to enable cursorline!
 
 o.clipboard = "unnamedplus"
 o.title = true
-o.hlsearch = false
 o.encoding = "utf-8"
 
 -- Colors
--- o.termguicolors = true
--- o.background = "dark"
+o.termguicolors = true
 
 -- Smooth scrolling
 o.smoothscroll = true
+o.scrolloff = 8
+o.colorcolumn = "80"
 
 -- Remember 50 items in commandline history
 o.history = 50
@@ -64,14 +62,15 @@ o.history = 50
 -- Preserve view while jumping
 o.jumpoptions = "view"
 
-vim.opt.mouse = "a" -- Enable mouse usage (all modes)
+o.mouse = "a" -- Enable mouse usage (all modes)
 
 -- Enable autocompletion:
 o.wildmode = "longest,list,full"
 
+o.updatetime = 50
+
 -- Set global variables
-g.hidden_all = 0
--- g.lightline = { colorscheme = 'gruvbox' }
+-- g.hidden_all = 0
 
 -- Set options
 o.showmode = true
@@ -80,6 +79,6 @@ o.ruler = true
 o.laststatus = 2
 
 -- Check if not running in a GUI
-if not vim.fn.has("gui_running") then
-  o.t_Co = 256
-end
+-- if not vim.fn.has("gui_running") then
+--   o.t_Co = 256
+-- end
