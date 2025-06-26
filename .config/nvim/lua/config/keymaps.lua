fb260d991d0965d04a0e13578a85c41583514c82
Agustin Gomez <niconecrofilo589@gmail.com>
2025-06-26 19:33:40 -0300

    Better remaps thanks theprimeagen

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-local opts = { silent = true }
+local opts = { noremap = true, silent = true }
 
 --Remap space as leader key
 map("", "<Space>", "<Nop>", opts)
@@ -20,12 +20,15 @@ map("i", "<C-A>", "<C-o>^")
 -- Thanks the Primeagen
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
+
 map("n", "J", "mzJ`z")
 
--- Quickly save the current buffer or all buffers
-map("n", "<leader>w", "<CMD>update<CR>", opts)
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
-map("n", "<leader>W", "<CMD>wall<CR>", opts)
+map("n", "<leader>x", "<cmd>!chmod +x %<CR>", opts)
 
 -- Navigate buffers
 -- map("n", "<S-l>", ":bnext<CR>", opts) -- la S significa shift
@@ -42,13 +45,13 @@ map("n", "<C-s>", "<CMD>w<CR>")
 map("n", "<C-z>", "<CMD>x<CR>")
 
 -- Create new tab
-map("n", "tn", "<CMD>tabnew<CR>", opts)
+map("n", "te", ":tabedit", opts)
 
 -- Close tab
-map("n", "tc", "<CMD>tabclose<CR>", opts)
+map("n", "<tab>", ":tabnext<Return>", opts)
 
 -- Get tabs
-map("n", "tt", "<CMD>tabs<CR>", opts)
+map("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Shortcut to yank register
 -- map({ "n", "x" }, "<leader>py", '"0p')
@@ -60,6 +63,9 @@ map("n", "tt", "<CMD>tabs<CR>", opts)
 -- ==>   Splits
 ------------------------------------------------
 
+map("n", "ss", ":split<Return>", opts)
+map("n", "vs", ":vsplit<Return>", opts)
+
 -- Shortcutting split navigation, saving a keypress:
 map("n", "<C-h>", "<C-w>h", opts)
 map("n", "<C-j>", "<C-w>j", opts)
@@ -67,10 +73,10 @@ map("n", "<C-k>", "<C-w>k", opts)
 map("n", "<C-l>", "<C-w>l", opts)
 
 -- Make adjusting split sizes a bit more friendly
--- map("n", "<C-Left>", "<CMD>vertical resize -2<CR>", opts)
--- map("n", "<C-Right>", "<CMD>vertical resize +2<CR>", opts)
--- map("n", "<C-Up>", "<CMD>resize -2<CR>", opts)
--- map("n", "<C-Down>", "<CMD>resize +2<CR>", opts)
+map("n", "<C-Left>", "<CMD>vertical resize -2<CR>", opts)
+map("n", "<C-Right>", "<CMD>vertical resize +2<CR>", opts)
+map("n", "<C-Up>", "<CMD>resize -2<CR>", opts)
+map("n", "<C-Down>", "<CMD>resize +2<CR>", opts)
 
 -- Change 2 split windows from vert to horiz or horiz to vert
 map("n", "<Leader>th", "<C-w>t<C-w>H")
@@ -78,9 +84,6 @@ map("n", "<Leader>tk", "<C-w>t<C-w>K")
 
 -- Replace ex mode with gq
 map("n", "Q", "gq")
-
--- Check file in shellcheck:
--- map("n", "<leader>s", ":!clear && shellcheck -x %<CR>")
 
 -- Compile document, be it groff/LaTeX/markdown/etc.
 map("n", "<leader>c", ':w! | !compiler "<c-r>%"<CR>')
@@ -92,7 +95,14 @@ map("n", "<leader>e", ":!pandoc % -t beamer -o %.pdf<CR>")
 map("x", ".", ":normal .<CR>")
 
 -- Mapping to open VimwikiIndex
-map("n", "<leader>ww", ":VimwikiIndex<CR>", { noremap = true })
+map("n", "<leader>ww", ":VimwikiIndex<CR>", opts)
 
 -- Control all select
 map("n", "<C-a>", "ggVG", opts)
+
+-- Start any lsp server if not already started
+map("n", "<C-l>", ":LspStart ")
+
+map("n", "<leader>rn", function()
+  return ":IncRename " .. vim.fn.expand("<cword>")
+end, { expr = true })
