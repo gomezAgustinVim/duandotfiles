c7d4e132c89c069b4c277bc31683c0634b39d1ab
Agustin Gomez <niconecrofilo589@gmail.com>
2025-07-04 12:41:01 -0300

    ui config

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/noice.nvim",
+  opts = function(_, opts)
+    table.insert(opts.routes, {
+      filter = {
+        event = "notify",
+        find = "No hay informacion duan nwn",
+      },
+      opts = { skip = true },
+    })
+
+    opts.presets.lsp_doc_border = true
+  end,
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 10000,
+    },
+  },
+}
