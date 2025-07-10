0e4c7d03349a45e3bda53fa889b7548c8c803e91
Agustin Gomez <niconecrofilo589@gmail.com>
2025-07-10 19:51:22 -0300

    Made hyprland more modular

--- .config/waybar/scripts/launch.sh
+++ .config/waybar/scripts/launch.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -9 waybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x waybar > /dev/null; do sleep 1; done
+while pgrep -u $UID -x waybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using config location ~/.config/bspwm/polybar/config.ini
 waybar &
