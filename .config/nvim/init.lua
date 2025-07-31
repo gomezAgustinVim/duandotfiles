2fb6f1d0626e3344fdd958fb063900a32f6d675f
Agustin Gomez <niconecrofilo589@gmail.com>
2025-07-31 01:23:01 -0300

    Installed some more plugins like tree sitter context

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,13 @@
 -- auto install vim-plug and plugins, if not found
 local data_dir = vim.fn.stdpath("data")
 if vim.fn.empty(vim.fn.glob(data_dir .. "/site/autoload/plug.vim")) == 1 then
-    vim.cmd(
-        "silent !curl -fLo "
-        .. data_dir
-        .. "/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-    )
-    vim.o.runtimepath = vim.o.runtimepath
-    vim.cmd("autocmd VimEnter * PlugInstall --sync | source $MYVIMRC")
+	vim.cmd(
+		"silent !curl -fLo "
+			.. data_dir
+			.. "/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+	)
+	vim.o.runtimepath = vim.o.runtimepath
+	vim.cmd("autocmd VimEnter * PlugInstall --sync | source $MYVIMRC")
 end
 
 local Plug = vim.fn["plug#"]
@@ -22,31 +22,31 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 Plug("ellisonleao/gruvbox.nvim", { ["as"] = "gruvbox" }) --colorscheme
-Plug("catppuccin/nvim", { ["as"] = "catppuccin" })       --colorscheme 2
-Plug("neovim/nvim-lspconfig")                            -- Native lsp
-Plug("nvim-lualine/lualine.nvim")                        --statusline
-Plug("vimwiki/vimwiki")                                  -- vimwiki
-Plug("norcalli/nvim-colorizer.lua")                      --color highlight
-Plug("MeanderingProgrammer/render-markdown.nvim")        --render md inline
-Plug("lewis6991/gitsigns.nvim")                          --git
-Plug("iamcco/markdown-preview.nvim")
-Plug("nvim-treesitter/nvim-treesitter")                  -- improved syntax
-Plug("echasnovski/mini.pairs")                           -- autopairs
-Plug("echasnovski/mini.snippets")                        -- snippets engine
-Plug("rafamadriz/friendly-snippets")                     -- snippets source
-Plug("echasnovski/mini.pick")                            -- File picker
-Plug("echasnovski/mini.icons")                           -- Icons 2
-Plug('nvim-tree/nvim-web-devicons')                      -- Icons
-Plug("nvim-tree/nvim-tree.lua")                          -- File explorer
-Plug("romgrk/barbar.nvim")                               --bufferline
-Plug("goolord/alpha-nvim")                               --pretty startup
+Plug("catppuccin/nvim", { ["as"] = "catppuccin" }) --colorscheme 2
+Plug("neovim/nvim-lspconfig") -- Native lsp
+Plug("nvim-lualine/lualine.nvim") --statusline
+Plug("vimwiki/vimwiki") -- vimwiki
+Plug("norcalli/nvim-colorizer.lua") --color highlight
+Plug("MeanderingProgrammer/render-markdown.nvim") --render md inline
+Plug("lewis6991/gitsigns.nvim") --git
+Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" }) -- improved syntax
+Plug("nvim-treesitter/nvim-treesitter-context") -- context of scope
+Plug("nvim-treesitter/nvim-treesitter-textobjects")
+Plug("echasnovski/mini.pairs") -- autopairs
+Plug("echasnovski/mini.snippets") -- snippets engine
+Plug("rafamadriz/friendly-snippets") -- snippets source
+Plug("echasnovski/mini.pick") -- File picker
+Plug("echasnovski/mini.icons") -- Icons 2
+Plug("nvim-tree/nvim-web-devicons") -- Icons
+Plug("nvim-tree/nvim-tree.lua") -- File explorer
+Plug("romgrk/barbar.nvim") --bufferline
+Plug("goolord/alpha-nvim") --pretty startup
+Plug("mfussenegger/nvim-lint") --async linter
+Plug("folke/which-key.nvim") --mappings popup
+Plug("stevearc/conform.nvim") -- better formatter (?
 -- Plug("L3MON4D3/LuaSnip")                                 -- more conventional snippets engine
 -- Plug("Saghen/blink.compat")                              -- compatibility with nvim.cmp
 -- Plug("Saghen/blink.cmp")                                 -- autocompletion
-Plug("mfussenegger/nvim-lint") --async linter
-Plug("folke/which-key.nvim")   --mappings popup
-Plug("stevearc/conform.nvim")  -- better formatter (?
---- Not configured yet ---
 
 vim.call("plug#end")
 
@@ -71,12 +71,12 @@ require("plugins.render-markdown")
 require("plugins.vimwiki")
 
 vim.defer_fn(function()
-    --defer non-essential configs,
-    --purely for experimental purposes:
-    --this only makes a difference of +-10ms on initial startup
-    require("plugins.mini-pairs")
-    require("plugins.treesitter")
-    require("plugins.mini-pick")
-    require("plugins.nvim-tree")
-    require("plugins.which-key")
+	--defer non-essential configs,
+	--purely for experimental purposes:
+	--this only makes a difference of +-10ms on initial startup
+	require("plugins.mini-pairs")
+	require("plugins.treesitter")
+	require("plugins.mini-pick")
+	require("plugins.nvim-tree")
+	require("plugins.which-key")
 end, 100)
