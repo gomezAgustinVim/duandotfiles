dcf7700c3ae03f1e8f59a0f198d2530f7c30fadd
Agustin Gomez <niconecrofilo589@gmail.com>
2025-08-10 19:51:29 -0300

    Native snippet path

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,12 +1,12 @@
 local ls = require("luasnip")
 
-vim.keymap.set({ "i" }, "<C-j>", function()
+vim.keymap.set({ "i" }, "<C-e>", function()
 	ls.expand()
 end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-l>", function()
+vim.keymap.set({ "i", "s" }, "<C-J>", function()
 	ls.jump(1)
 end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-h>", function()
+vim.keymap.set({ "i", "s" }, "<C-K>", function()
 	ls.jump(-1)
 end, { silent = true })
 
@@ -17,3 +17,4 @@ vim.keymap.set({ "i", "s" }, "<C-y>", function()
 end, { silent = true })
 
 require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
