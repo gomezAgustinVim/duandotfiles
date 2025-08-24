ff38fa7d9a9e5b54d866d42d0abc8763fc10958b
Agustin Gomez <niconecrofilo589@gmail.com>
2025-08-23 21:44:12 -0300

    yaml parser

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -12,17 +12,9 @@ require("nvim-treesitter.configs").setup({
 		"lua",
 		"markdown",
 		"markdown_inline",
+		"yaml",
 	},
 	highlight = {
 		enable = true,
-	},
-	incremental_selection = {
-		enable = false,
-		keymaps = {
-			init_selection = "gnn", -- set to `false` to disable one of the mappings
-			node_incremental = "grn",
-			scope_incremental = "grc",
-			node_decremental = "grm",
-		},
 	},
 })
