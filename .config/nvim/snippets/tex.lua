2ef3651ced457c7ed9a4d959924ba531473506b5
Agustin Gomez <niconecrofilo589@gmail.com>
2025-09-01 23:17:15 -0300

    Added snippets for math mode and subscript with functions

--- .config/nvim/snippets/tex.lua
+++ .config/nvim/snippets/tex.lua
@@ -16,7 +16,6 @@ return {
 			{ i(1, "filename"), i(2, "captionBold"), i(3, "captionText"), i(4, "figureLabel") }
 		)
 	),
-
 	s(
 		{ trig = "beg", snippetType = "autosnippet" },
 		fmta(
@@ -38,5 +37,65 @@ return {
         ]],
 			{ i(1, "itemize"), i(2), rep(1) }
 		)
+	),
+
+	s(
+		{ trig = "bold", snippetType = "autosnippet" },
+		fmta(
+			[[
+        \textbf{<>}
+        ]],
+			{ i(1) }
+		)
+	),
+
+	-- math modes
+	s(
+		{ trig = "ml", snippetType = "autosnippet" },
+		fmta(
+			[[
+            $<>$
+        ]],
+			{ i(1) }
+		)
+	),
+	s(
+		{ trig = "mb", snippetType = "autosnippet" },
+		fmta(
+			[[
+            \[
+                <>
+            \]
+        ]],
+			{ i(1) }
+		)
+	),
+	s(
+		{ trig = "frac", snippetType = "autosnippet" },
+		fmta(
+			[[
+        \frac{<>}{<>}
+        ]],
+			{ i(1), i(2) }
+		)
+	),
+
+	-- sub super scripts
+	s(
+		{ trig = "(%a)(%d)", regTrig = true, name = "auto subscript" },
+		f(function(_, snip)
+			return snip.captures[1] .. "_" .. snip.captures[2]
+		end, { condition = math })
+	),
+	s(
+		{ trig = "(%a)(%d%d)", regTrig = true, name = "auto subscript 2", dscr = "auto subscript for 2+ digits" },
+		fmt([[<>_{<>}]], {
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			f(function(_, snip)
+				return snip.captures[2]
+			end),
+		}, { delimiters = "<>", condition = math })
 	),
 }
