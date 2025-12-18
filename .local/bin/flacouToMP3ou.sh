b3e810818fb9a4f8690d9aa1b4add2d8be84b617
Agustin Gomez <niconecrofilo589@gmail.com>
2025-12-18 13:54:18 -0300

    Add conversion of aiff

--- .local/bin/flacouToMP3ou.sh
+++ .local/bin/flacouToMP3ou.sh
@@ -4,7 +4,7 @@ echo "Desea convertir todos los directorios y subdirectorios a mp3?"
 read -p "'y' para sí, 'n' para no: " rta
 if [[ $rta = 'y' ]]; then
     # El que encontré en internet
-    find . -type f -name '*.flac' -print -exec sh -c 'i="{}"; ffmpeg -i "$i" -y -b:a 320k "${i%.flac}".mp3 && rm -f "$i"' \;
+    find . -type f \( -name '*.flac' -o -name '*.aiff' \) -print -exec sh -c 'i="{}"; base="${i%.*}"; ffmpeg -i "$i" -y -b:a 320k "${base}.mp3" && rm -f "$i"' \;
     notify-send "Conversion de flac a mp3 completada nwn"
 elif [[ $rta = 'n' ]]; then
     echo "Chau"
