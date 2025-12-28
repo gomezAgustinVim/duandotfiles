ba3d8461970429a872e647c22d42694cb8c6dde5
Agustin Gomez <niconecrofilo589@gmail.com>
2025-12-28 19:37:09 -0300

    Refactor de variables y agregado check de si hay mas de un directorio

--- .local/bin/backupPublicShare.sh
+++ .local/bin/backupPublicShare.sh
@@ -1,46 +1,58 @@
 #!/bin/sh
 
-# Works with runit
 # Parameters for RSYNC
 COPIADOS="$@"
+
 # My filter rules
 BACKUP_FILTER="$HOME/Documentos/backupPublic.filter"
+
 # File to store backup history
-DAY=$(date +%y%d%m-%H-%M)
+FECHA_HORA=$(date +%y%d%m-%H-%M)
 # USB PATH GOES HERE
-DESTINO="$HOME/Publico/backup"
-DESTINO_FINAL="$DESTINO/$DAY"
+BACKUPS="$HOME/Publico/backup"
+DESTINO="$BACKUPS/$FECHA_HORA"
+
 # Backup history file
 BACKUP_HISTORY_FILE="/tmp/backup_history.txt"
 
-# Get the oldest backup directory
-ls -t1r "$DESTINO" | head -n 1 > $BACKUP_HISTORY_FILE
-BACKUP_HISTORY_CONTENT=$(cat $BACKUP_HISTORY_FILE)
+# Count directories in BACKUPS
+DIR_COUNT=$(find "$BACKUPS" -maxdepth 1 -type d | wc -l)
+# Subtract 1 for the BACKUPS directory itself
+DIR_COUNT=$((DIR_COUNT - 1))
 
-if [ -z "$BACKUP_HISTORY_CONTENT" ]; then
-    notify-send "Aun no hay un directorio creado en $DESTINO." "Creando..."
-    mkdir -p $DESTINO_FINAL
+if [ $DIR_COUNT -gt 1 ]; then
+    notify-send "Hay más de un directorio en $BACKUPS." "Sólo puede haber uno"
+    exit 1
+else
+    # Get the oldest backup directory
+    # ls -t1r "$BACKUPS" | head -n 1 > $BACKUP_HISTORY_FILE
+    ls "$BACKUPS" > $BACKUP_HISTORY_FILE
+    ORIGEN=$(cat $BACKUP_HISTORY_FILE)
+fi
+
+if [ -z "$ORIGEN" ]; then
+    notify-send "Aun no hay un directorio creado en $BACKUPS." "Creando..."
+    mkdir -p "$DESTINO"
     notify-send "Directorio creado con éxito nwn"
 fi
-#
+
 # Append the current backup directory to the history file
-echo "$DAY" >> $BACKUP_HISTORY_FILE
+echo "$FECHA_HORA" >> $BACKUP_HISTORY_FILE
 
 # Get the previous backup directory from the history file
-DIRECTORIO_PREVIO=$(head -n 1 $BACKUP_HISTORY_FILE)
-DIRECTORIO_ACTUAL=$(tail -n 1 $BACKUP_HISTORY_FILE)
+ACTUAL=$(tail -n 1 $BACKUP_HISTORY_FILE)
 
 # The reason I do this is because I don't care about incr backups
 # It's not really necesary for my use case
-mv "$DESTINO/$DIRECTORIO_PREVIO" "$DESTINO_FINAL"
+mv "$BACKUPS/$ORIGEN" "$DESTINO"
 
-rsync -Pav -z --update --delete-after --filter="merge $BACKUP_FILTER" $COPIADOS $DESTINO_FINAL
+rsync -Pav -z --update --delete-after --filter="merge $BACKUP_FILTER" $COPIADOS $DESTINO
 
 # Clean up the backup history file
 rm $BACKUP_HISTORY_FILE
 
 if [ $? -eq 0 ]; then
-    notify-send "Backup completado" "Los archivos se han copiado a $DESTINO_FINAL"
+    notify-send "Backup completado" "Los archivos se han copiado a $DESTINO"
 else
-    notify-send "Error en el backup" "Hubo un problema al copiar los archivos a $DESTINO_FINAL"
+    notify-send "Error en el backup" "Hubo un problema al copiar los archivos a $DESTINO"
 fi
