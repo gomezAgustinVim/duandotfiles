e174b5cf4663ed2d2b47c12781e603e2b1ec0537
Agustin Gomez <niconecrofilo589@gmail.com>
2026-03-12 00:31:14 -0300

    Formatting done with none-ls now

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,26 @@
 vim.pack.add({
-    { src = "https://github.com/ellisonleao/gruvbox.nvim",                 name = "gruvbox" }, --colorscheme
-    { src = "https://github.com/vimwiki/vimwiki" },                            -- vimwiki
-    { src = "https://github.com/norcalli/nvim-colorizer.lua" },                --color highlight
-    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },  --render md inline
-    { src = "https://github.com/lewis6991/gitsigns.nvim" },                    --git
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },            -- improved syntax
-    { src = "https://github.com/nvimtools/none-ls.nvim" },                     -- linter
-    { src = "https://github.com/nvim-lua/plenary.nvim" },                      -- dependency for linter
-    { src = "https://github.com/echasnovski/mini.pairs" },                     -- autopairs
-    { src = "https://github.com/echasnovski/mini.surround" },                  -- surround words with whatever
-    { src = "https://github.com/echasnovski/mini.completion" },                -- completion menu with snippets
-    { src = "https://github.com/echasnovski/mini.icons" },                     -- Icons
-    { src = "https://github.com/stevearc/oil.nvim" },                          -- Explorer with buffer like edit of directory structure
-    { src = "https://github.com/refractalize/oil-git-status.nvim" },           -- git changes on oil
-    { src = "https://github.com/goolord/alpha-nvim" },                         --pretty startup
-    { src = "https://github.com/folke/which-key.nvim" },                       --mappings popup
-    { src = "https://github.com/L3MON4D3/LuaSnip" },                           -- more conventional snippets engine
-    { src = "https://github.com/lukas-reineke/indent-blankline.nvim" },        -- self explanatory
-    { src = "https://github.com/ibhagwan/fzf-lua" },                           -- manage files, buffers, git and grepper
-    { src = "https://github.com/mason-org/mason.nvim" },                       -- look for lsps and formatters in one place
-    { src = "https://github.com/supermaven-inc/supermaven-nvim" },             -- super useful AI
-    -- { src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/ellisonleao/gruvbox.nvim", name = "gruvbox" }, --colorscheme
+	{ src = "https://github.com/vimwiki/vimwiki" }, -- vimwiki
+	{ src = "https://github.com/norcalli/nvim-colorizer.lua" }, --color highlight
+	{ src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" }, --render md inline
+	{ src = "https://github.com/lewis6991/gitsigns.nvim" }, --git
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" }, -- improved syntax
+	{ src = "https://github.com/nvimtools/none-ls.nvim" }, -- linter
+	{ src = "https://github.com/nvim-lua/plenary.nvim" }, -- dependency for linter
+	{ src = "https://github.com/echasnovski/mini.pairs" }, -- autopairs
+	{ src = "https://github.com/echasnovski/mini.surround" }, -- surround words with whatever
+	{ src = "https://github.com/echasnovski/mini.completion" }, -- completion menu with snippets
+	{ src = "https://github.com/echasnovski/mini.icons" }, -- Icons
+	{ src = "https://github.com/stevearc/oil.nvim" }, -- Explorer with buffer like edit of directory structure
+	{ src = "https://github.com/refractalize/oil-git-status.nvim" }, -- git changes on oil
+	{ src = "https://github.com/goolord/alpha-nvim" }, --pretty startup
+	{ src = "https://github.com/folke/which-key.nvim" }, --mappings popup
+	{ src = "https://github.com/L3MON4D3/LuaSnip" }, -- more conventional snippets engine
+	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim" }, -- self explanatory
+	{ src = "https://github.com/ibhagwan/fzf-lua" }, -- manage files, buffers, git and grepper
+	{ src = "https://github.com/mason-org/mason.nvim" }, -- look for lsps and formatters in one place
+	{ src = "https://github.com/supermaven-inc/supermaven-nvim" }, -- super useful AI
+	-- { src = "https://github.com/neovim/nvim-lspconfig" },
 })
 
 -- move config and plugin config to alternate files
@@ -39,9 +39,9 @@ require("plugins.which-key")
 require("plugins.luasnip")
 
 require("fzf-lua").setup({
-    lsp = {
-        async_or_timeout = 3000,
-    },
+	lsp = {
+		async_or_timeout = 3000,
+	},
 })
 
 require("mini.completion").setup()
@@ -51,16 +51,16 @@ require("mini.surround").setup()
 require("mini.icons").setup()
 
 require("oil").setup({
-    win_options = {
-        signcolumn = "yes:2",
-        wrap = false,
-        cursorcolumn = false,
-        foldcolumn = "0",
-        spell = false,
-        list = false,
-        conceallevel = 3,
-        concealcursor = "nvic",
-    },
+	win_options = {
+		signcolumn = "yes:2",
+		wrap = false,
+		cursorcolumn = false,
+		foldcolumn = "0",
+		spell = false,
+		list = false,
+		conceallevel = 3,
+		concealcursor = "nvic",
+	},
 })
 
 require("oil-git-status").setup()
@@ -70,46 +70,45 @@ require("gruvbox").setup({ transparent_mode = true })
 require("ibl").setup()
 
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-        },
-    },
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 require("supermaven-nvim").setup({
-    keymaps = {
-        accept_suggestion = "<S-Tab>",
-        clear_suggestion = "<C-]>",
-        accept_word = "<C-w>",
-    },
-    ignore_filetypes = { cpp = true, markdown = true },
-    color = {
-        suggestion_color = "#008282", -- color for the suggestion text
-        cterm = 244,
-    },
-    log_level = "off",                 -- set to "off" to disable logging completely
-    disable_inline_completion = false, -- disables inline completion for use with cmp
-    disable_keymaps = false,           -- disables built in keymaps for more manual control
-    condition = function()
-        return false
-    end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+	keymaps = {
+		accept_suggestion = "<S-Tab>",
+		clear_suggestion = "<C-]>",
+		accept_word = "<C-w>",
+	},
+	ignore_filetypes = { cpp = true, markdown = true },
+	color = {
+		suggestion_color = "#008282", -- color for the suggestion text
+		cterm = 244,
+	},
+	log_level = "off", -- set to "off" to disable logging completely
+	disable_inline_completion = false, -- disables inline completion for use with cmp
+	disable_keymaps = false, -- disables built in keymaps for more manual control
+	condition = function()
+		return false
+	end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
 })
 
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.completion.spell,
-        null_ls.builtins.completion.luasnip,
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.code_actions.refactoring,
-    }
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.completion.luasnip,
+		null_ls.builtins.code_actions.gitsigns,
+		null_ls.builtins.code_actions.refactoring,
+	},
 })
 
 vim.cmd("silent! colorscheme gruvbox")
--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,73 +1,73 @@
 -- Custom filetypes
 vim.filetype.add({
-    extension = {
-        conf = "conf",
-        mdx = "markdown",
-        rmd = "markdown",
-        Rmd = "markdown",
-        mjml = "html",
-        kicad_mod = "scheme",
-    },
-    pattern = {
-        [".*%.env.*"] = "sh",
-        ["ignore$"] = "conf",
-    },
-    filename = {
-        ["yup.lock"] = "yaml",
-    },
+	extension = {
+		conf = "conf",
+		mdx = "markdown",
+		rmd = "markdown",
+		Rmd = "markdown",
+		mjml = "html",
+		kicad_mod = "scheme",
+	},
+	pattern = {
+		[".*%.env.*"] = "sh",
+		["ignore$"] = "conf",
+	},
+	filename = {
+		["yup.lock"] = "yaml",
+	},
 })
 
 local num_au = vim.api.nvim_create_augroup("NUMTOSTR", { clear = true })
 
 -- disable automatic comment on newline
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
-    callback = function()
-        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-    end,
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
 })
 
 -- Autoformat on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        local clients = vim.lsp.get_clients({ bufnr = args.buf })
-        if #clients > 0 then
-            vim.lsp.buf.format({ async = false })
-        end
-    end,
+	pattern = "*",
+	callback = function(args)
+		local clients = vim.lsp.get_clients({ bufnr = args.buf })
+		if #clients > 0 then
+			vim.lsp.buf.format({ async = false })
+		end
+	end,
 })
 
 -- highlight text on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-    pattern = "*",
-    callback = function()
-        vim.highlight.on_yank({ timeout = 300 })
-    end,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ timeout = 300 })
+	end,
 })
 
 -- Remove useless stuff from the terminal window and enter INSERT mode
 vim.api.nvim_create_autocmd("TermOpen", {
-    group = num_au,
-    callback = function(data)
-        if not string.find(vim.bo[data.buf].filetype, "^[fF][tT]erm") then
-            vim.api.nvim_set_option_value("number", false, { scope = "local" })
-            vim.api.nvim_set_option_value("relativenumber", false, { scope = "local" })
-            vim.api.nvim_set_option_value("signcolumn", "no", { scope = "local" })
-            vim.api.nvim_command("startinsert")
-        end
-    end,
+	group = num_au,
+	callback = function(data)
+		if not string.find(vim.bo[data.buf].filetype, "^[fF][tT]erm") then
+			vim.api.nvim_set_option_value("number", false, { scope = "local" })
+			vim.api.nvim_set_option_value("relativenumber", false, { scope = "local" })
+			vim.api.nvim_set_option_value("signcolumn", "no", { scope = "local" })
+			vim.api.nvim_command("startinsert")
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("my.lsp", {}),
-    callback = function(ev)
-        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+	group = vim.api.nvim_create_augroup("my.lsp", {}),
+	callback = function(ev)
+		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
 
-        if client:supports_method("textDocument/completion") then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-        end
-    end,
+		if client:supports_method("textDocument/completion") then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
 })
 
 ------------------------------------------------------------------------------
