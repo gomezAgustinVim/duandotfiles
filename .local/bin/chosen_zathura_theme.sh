3369d9813699b6de71fd121b8277c52d3ec411ab
lordagustin22 <niconecrofilo589@gmail.com>
2026-03-17 00:23:06 -0300

    Refactor script de elegir tema de zathura

--- .local/bin/chosen_zathura_theme.sh
+++ .local/bin/chosen_zathura_theme.sh
@@ -1,28 +1,24 @@
 #!/bin/sh
 
+set -e 
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+error_msg() {
+    printf "${RED}ERROR:" "${NC} %s\n$1" >&2
+    exit 1
+}
+
 # Define the path to your Zathura configuration file
-CONFIG_FILE="${XDG_CONFIG_HOME:-$HOME/.config}/zathura/zathura-theme"
+ZATHURA_CONF="${XDG_CONFIG_HOME:-$HOME/.config}/zathura/"
+THEME_DIR="$ZATHURA_CONF/themes"
+THEMES="$(ls $THEME_DIR)" > /dev/null 2>&1 || error_msg "No hay temas"
 
-# Define the lines for the light and dark themes
-LIGHT_THEME="zathura-gruvbox-light"
-DARK_THEME="zathura-gruvbox-dark"
+MENU=$(echo "$THEMES" | rofi -dmenu -p "Temas disponibles")
 
-# Escapar caracteres especiales para usar en sed
-# Esto haría falta usarlo si y solo si utilizara la ruta completa en la variable
-# Es decir si hicieramos: include ./ y el resto del comando
-# Esto es porque sed interpretaria al punto y a la barra como caracteres especiales
-# Y tendriamos que escapar toda la secuencia completa de posibles caracteres especiales
-# En cuyo caso, ni siquiera vale la pena utilizar
-# $, *, ^ o [], ya que los únicos que dan problema son . y /
-#ESCAPED_LIGHT_THEME=$(printf '%s\n' "$LIGHT_THEME" | sed 's/[]\/$*.^[]/\\&/g')
-#ESCAPED_DARK_THEME=$(printf '%s\n' "$DARK_THEME" | sed 's/[]\/$*.^[]/\\&/g')
+THEME=$(echo "$MENU")
 
-if grep -q "$LIGHT_THEME" "$CONFIG_FILE"; then
-    sed -i "/$LIGHT_THEME/d" "$CONFIG_FILE"
-    echo 'include ./'"$DARK_THEME" >>"$CONFIG_FILE"
-    dunstify -u normal "Cambiado al tema oscuro"
-else
-    sed -i "/$DARK_THEME/d" "$CONFIG_FILE"
-    echo 'include ./'"$LIGHT_THEME" >>"$CONFIG_FILE"
-    dunstify -u normal "Cambiado al tema claro"
-fi
+echo "Cambiando tema de zathura"
+ln -sf "$THEME_DIR/$THEME" "$ZATHURA_CONF/zathura-theme"
+
