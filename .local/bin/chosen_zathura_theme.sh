f49a523f798000358624330c0cab104aa44891bd
lordagustin22 <niconecrofilo589@gmail.com>
2026-03-17 00:39:47 -0300

    Refactor script de elegir tema de zathura

--- .local/bin/chosen_zathura_theme.sh
+++ .local/bin/chosen_zathura_theme.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-set -e 
+set -e
 
 RED='\033[0;31m'
 NC='\033[0m'
 
 error_msg() {
-    printf "${RED}ERROR:" "${NC} %s\n$1" >&2
-    exit 1
+	printf "${RED}ERROR:" "${NC} %s\n$1" >&2
+	exit 1
 }
 
 # Define the path to your Zathura configuration file
 ZATHURA_CONF="${XDG_CONFIG_HOME:-$HOME/.config}/zathura/"
 THEME_DIR="$ZATHURA_CONF/themes"
-THEMES="$(ls $THEME_DIR)" > /dev/null 2>&1 || error_msg "No hay temas"
+THEMES="$(ls $THEME_DIR)" >/dev/null 2>&1 || error_msg "No hay temas"
 
 MENU=$(echo "$THEMES" | rofi -dmenu -p "Temas disponibles")
 
@@ -21,4 +21,3 @@ THEME=$(echo "$MENU")
 
 echo "Cambiando tema de zathura"
 ln -sf "$THEME_DIR/$THEME" "$ZATHURA_CONF/zathura-theme"
-
