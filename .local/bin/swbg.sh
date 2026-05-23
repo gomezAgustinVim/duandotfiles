642c015469a0b22a610a4aacb2b7d428e903807b
lordagustin22 <niconecrofilo589@gmail.com>
2026-05-23 20:34:37 -0300

    Rewritten setbg and swbg.sh scripts

--- .local/bin/swbg.sh
+++ .local/bin/swbg.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-CURRENT_WALL="$(basename "$(readlink -f ~/.local/share/wall.*)")"
+BGLOC="${XDG_DATA_HOME:-$HOME/.local/share}/bg"
+CURRENT_WALL="$(readlink -f "$BGLOC"/wall.*)"
 
 killall -9 swaybg
 
 WALLPAPER_DIR="${XDG_PICTURES_DIR:-$HOME/Imágenes}/walls/nord/"
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$CURRENT_WALL" | shuf -n 1)
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 EXT="$(basename "${WALLPAPER##*.}")"
-ln -sf "$WALLPAPER" "$HOME/.local/share/wall.$EXT"
+ln -sf "$WALLPAPER" "$BGLOC"/wall."$EXT"
 swaybg -i "$WALLPAPER"
