5b80584e2221767567768affe3a9018d325dac35
lordagustin22 <niconecrofilo589@gmail.com>
2026-05-28 22:33:01 -0300

    Better handling of conversion and POSIX compliant

--- .local/bin/flacouToMP3ou.sh
+++ .local/bin/flacouToMP3ou.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Desea convertir todos los directorios y subdirectorios a mp3?"
-read -p "'y' para sí, 'n' para no: " rta
-if [[ $rta = 'y' ]]; then
-    # El que encontré en internet
-    find . -type f \( -name '*.flac' -o -name '*.aiff' \) -print -exec sh -c 'i="{}"; base="${i%.*}"; ffmpeg -i "$i" -y -b:a 320k "${base}.mp3" && rm -f "$i"' \;
-    notify-send "Conversion de flac a mp3 completada nwn"
-elif [[ $rta = 'n' ]]; then
-    echo "Chau"
-    exit 0
-fi
+printf "'s' para sí, 'n' para no: "
+read -r rta
+case "$rta" in
+[Ss])
+	find . -type f \( -name '*.flac' -o -name '*.aiff' \) -print \
+		-exec sh -c \
+		'i="$1"; ffmpeg -loglevel warning -i "$i" -y -b:a 320k "${i%.*}.mp3" && rm -f "$i"' \
+		_ {} \;
+	notify-send "Conversion de flac a mp3 completada nwn"
+	;;
+[Nn]) exit 0 ;;
+*)
+	echo "Opción invalida" >&2
+	exit 1
+	;;
+esac
